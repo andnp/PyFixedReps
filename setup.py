58f4c2ccc47e884990c09d3483dbb800f9284ac1
andy <andnpatterson@gmail.com>
2019-05-08 19:29:15 -0600

    add numpy as dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Andy Patterson',
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
-    install_requires=[],
+    install_requires=[ 'numpy' ],
     version=0.1,
     license='MIT',
     description='A small set of fixed representations usually used in RL',
