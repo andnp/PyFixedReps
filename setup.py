813a8c1d81eaa7be7503f85c4859575f52692846
Travis CI User <travis@example.org>
2020-12-11 19:09:32 +0000

    updating to version 0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='andnpatterson@gmail.com',
     packages=find_packages(exclude=['tests*']),
     install_requires=[ 'numpy', 'numba' ],
-    version=0.4,
+    version=0.5,
     license='MIT',
     description='A small set of fixed representations usually used in RL',
     long_description='todo',
