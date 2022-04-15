d3ca763b782086eddfef14859ec4902383629df7
github-action <andnpatterson@gmail.com>
2022-04-15 19:23:26 +0000

    bump: version 0.0.5 → 1.0.0

--- setup.py
+++ setup.py
@@ -4,16 +4,27 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='PyFixedReps-andnp',
-    url='https://github.com/andnp/PyFixedReps.git',
-    author='Andy Patterson',
-    author_email='andnpatterson@gmail.com',
-    packages=find_packages(exclude=['tests*']),
-    version='0.0.5',
-    license='MIT',
-    description='A small set of fixed representations usually used in RL',
+    extras_require={
+        "dev": [
+            "flake8",
+            "mypy",
+            "commitizen",
+            "pre-commit",
+            "pipenv-setup[black]",
+            "twine",
+            "build",
+        ]
+    },
+    name="PyFixedReps-andnp",
+    url="https://github.com/andnp/PyFixedReps.git",
+    author="Andy Patterson",
+    author_email="andnpatterson@gmail.com",
+    packages=find_packages(exclude=["tests*"]),
+    version="1.0.0",
+    license="MIT",
+    description="A small set of fixed representations usually used in RL",
     long_description=long_description,
     long_description_content_type="text/markdown",
     python_requires=">=3.6",
-    install_requires=[ 'numpy', 'numba' ],
+    install_requires=["numba>=0.52.0", "numpy>=1.21.0"],
 )
