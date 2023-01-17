7536f4bd78cc66fb3408da02fdbfc088bfb203b8
andy <andnpatterson@gmail.com>
2023-01-17 15:50:00 -0700

    ci: update ci to use pdm

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
@@ -10,16 +11,12 @@ git fetch --all --tags
 
 git checkout -f master
 
-# sync package dependencies
-pipenv-setup sync --dev --pipfile
-
 # bump the version
 cz bump --no-verify --yes --check-consistency
 
+# push to pypi repository
+pdm build
+python -m twine upload -u __token__ -p ${PYPI_TOKEN} --non-interactive dist/*
+
 git push
 git push --tags
-
-# push to pypi repository
-python -m build
-
-python -m twine upload -u __token__ -p ${PYPI_TOKEN} --non-interactive dist/*
