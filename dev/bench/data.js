7f68954d1dc0162a9f36a74f1de0101c48ea2c28
github-action-benchmark <github@users.noreply.github.com>
2024-03-03 23:31:11 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for e09f09e42d701a13c6d8238c73af95f40e655c5f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709508282744,
+  "lastUpdate": 1709508671761,
   "repoUrl": "https://github.com/andnp/PyFixedReps",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 4.3746636905477034e-7",
             "extra": "mean: 4.382207600697487 usec\nrounds: 16209"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "andnpatterson@gmail.com",
+            "name": "Andy Patterson",
+            "username": "andnp"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e09f09e42d701a13c6d8238c73af95f40e655c5f",
+          "message": "chore: add benchmarks to readme",
+          "timestamp": "2024-03-03T16:30:17-07:00",
+          "tree_id": "9f10ac9aa5fb21cc1983c4be32a8eed951fbdfa4",
+          "url": "https://github.com/andnp/PyFixedReps/commit/e09f09e42d701a13c6d8238c73af95f40e655c5f"
+        },
+        "date": 1709508670887,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/test_TileCoder.py::test_benchmark_encode",
+            "value": 231214.48872664204,
+            "unit": "iter/sec",
+            "range": "stddev: 5.008836046408854e-7",
+            "extra": "mean: 4.324988479343395 usec\nrounds: 24304"
           }
         ]
       }
