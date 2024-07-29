4a8220da18736e3ec57a34573ada8f56f0b9713a
andnp <andnpatterson@gmail.com>
2024-07-29 09:42:53 -0600

    fix: make numpy v2 compatible

--- PyFixedReps/RBF.py
+++ PyFixedReps/RBF.py
@@ -2,10 +2,10 @@ import numpy as np
 import numpy.typing as npt
 from typing import Optional
 from PyFixedReps._jit import try2jit
-from PyFixedReps.BaseRepresentation import Array, BaseRepresentation
+from PyFixedReps.BaseRepresentation import BaseRepresentation
 
 @try2jit
-def gaussian_dist(x: Array, centers: Array, width: float):
+def gaussian_dist(x: np.ndarray, centers: np.ndarray, width: float):
     diff = x - centers
     squared = np.sum(np.square(diff), axis=1)
     features = np.exp(-1 * squared / np.square(width))
